0336c119d9a224081bc8bfd6a612dc6d9604b448
tominaga1653 <108568710+tominaga1653@users.noreply.github.com>
2022-07-31 03:09:21 +0000

    [Add] 閲覧数

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,8 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
+  has_many :view_counts, dependent: :destroy
+
   has_one_attached :profile_image
 
 
